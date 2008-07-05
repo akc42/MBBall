a6b79aacaf857024ca2dbf25a14d5e40e23f59de
Alan Chandler <alan@chandlerfamily.org.uk>
2008-07-05 20:49:34 +0100

    Update index page to now properly display menus

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ define('MBBALL_ICON_PATH', '/football/images/');
 $groups =& $user_info['groups'];
 $uid = $ID_MEMBER;
 $name =& $user_info['name'];
-$password = ha1("Key".$uid);
+$password = sha1("Key".$uid);
 
 define ('BALL',1);   //defined so we can control access to some of the files.
 require_once('db.php');
@@ -37,7 +37,7 @@ if(isset($_GET['cid']) {
 		$cid = $row['cid'];
 	} else {
 		if(in_array(SMF_FOOTBALL,$groups)) {
-			header( 'Location: http://www.melindasbackups.com/football/admin.php?uid='.$uid.'&pass='.$password ) ; //No CID means general. 
+			header( 'Location: http://www.melindasbackups.com/football/admin.php?uid='.$uid.'&pass='.$password.'&global=true' ) ; 
 		} else {
 			header( 'Location: http://www.melindasbackups.com/football/nostart.html' ) ;
 		};
@@ -50,7 +50,7 @@ $result = dbQuery('SELECT * FROM Competition WHERE cid = '.dbMakeSafe($cid).';')
 if (dbNumRows($result) == 0) {
 	//This competition doesn't exist yet
 	if(in_array(SMF_FOOTBALL,$groups)) {
-		header( 'Location: http://www.melindasbackups.com/football/admin.php?uid='.$uid.'&pass='.$password.'&cid='.$cid.'&admin=true' ) ;
+		header( 'Location: http://www.melindasbackups.com/football/admin.php?uid='.$uid.'&pass='.$password.'&global=true.'&cid='.$cid' ) ;
 	} else {
 		header( 'Location: http://www.melindasbackups.com/football/nostart.html' ) ;
 	};
@@ -62,21 +62,8 @@ if ($uid == $row['administrator']) {
 	//User is administrator of this competition
 	header( 'Location: http://www.melindasbackups.com/football/admin.php?uid='.$uid.'&pass='.$password.'&cid='.$cid );
 	exit;
-} 
-
-//Check if this competition is open and ready yet
-$result = dbQuery('SELECT * FROM Open_Competition WHERE cid = '.dbMakeSafe($cid).';');
-if (dbNumRows($result) == 0 ) {
-	//See if I am the adminisat
-	header( 'Location: http://www.melindasbackups.com/football/nostart.html' ) ;
-	exit;
 }
-	
-
-
-if(in_array(SMF_FOOTBALL,$groups)) {
-// Add Admin Link to menu section - this whole if clause has to move into menu area
-};
+dbFree($result)
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
@@ -85,7 +72,7 @@ if(in_array(SMF_FOOTBALL,$groups)) {
 	<title>Melinda's Backups Chat</title>
 	<link rel="stylesheet" type="text/css" href="ball.css" title="mbstyle"/>
 	<!--[if lt IE 7]>
-		<link rel="stylesheet" type="text/css" href="chat-ie.css"/>
+		<link rel="stylesheet" type="text/css" href="ball-ie.css"/>
 	<![endif]-->
 	<script src="/static/scripts/mootools-1.2-core.js" type="text/javascript" charset="UTF-8"></script>
 	<script src="mbball.js" type="text/javascript" charset="UTF-8"></script>
@@ -119,14 +106,120 @@ window.addEvent('unload', function() {
 	</td>
 		<td align="right" width="25" class="topbg_r2" valign="top">
 		<div id="competitionNameContainer">
-			<h1><?php echo "This will Eventually Contain Competition Name from Database" ?></h1>
+			<h1><?php echo $row['description'] ?></h1>
 		</div>
 		<!-- blank -->
 		</td>
 	</tr>  </tbody>
 </table>
+<?php
 
-<div id="content">
+
+
+
+$menu = false;
+//Are we registered for the competition yet?
+$result = dbQuery('SELECT * FROM registration 
+			WHERE uid = '.dbMakeSafe($uid).' AND cid = '.dbMakeSafe($cid).';');
+if(dbNumRows($result) == 0) {
+	//We are not registered for this competition
+	$registered = false;
+
+	// Does competition allow registration at this time
+	if($row['open'] == 't') {
+		//yes so provide a link to register this user
+?><ul id="menu">
+	<li><a id="register" href="register.php?<?php echo 'uid='.$uid.'&pass='.$password.'&cid='.$cid; ?>">Register	</a></li>
+<?php
+		$menu=true;
+	}
+} else {
+	$registered = true;
+}
+dbFree($result);
+dbQuery('SELECT DISTINCT round.rid round.name FROM match JOIN round USING (cid,rid) WHERE cid = '
+		.dbMakeSafe($cid).' AND status > '.(($registered)? '1' : '2').'ORDER BY rid DESC ;');
+if (dbNumRows($result) > 0) {
+	$row=dbFetchRow($result);
+
+	if(isset($_GET['rid']))
+		$rid=$_GET['rid'];
+	} else {
+		$rid=$row['rid'];  //if not set use the first row (ie highest round)
+	}
+
+	if (dbNumRows($result) >1 ) {
+		// more than one round, so we need to have a menu for the others
+		if (!$menu) {
+?><ul id="menu">
+<?php
+		$menu=true;
+		}
+?>	<li>Rounds
+		<ul>
+<?php 
+
+		do {
+			if ($row['rid'] != $rid) {
+?>			<li><a href="index.php?<?php echo 'cid='.$cid.'&rid='.$row['rid']; ?>"><?php echo $row['name'] ;?></a></li>
+<?php
+			}
+		} while ($row = dbFetchRow($result));
+?>		</ul>
+	</li>
+<?php
+	}
+}
+dbFreeResult($result);
+
+// The following select should select the cid and name of all competitions that are in a state where 
+$sql = 'SELECT cid, name FROM competition WHERE cid <> '.dbMakeSafe($cid).' AND open IS TRUE AND cid NOT IN ';
+// checking not already registered for a cid where registration is only open
+$sql .= '(SELECT cid FROM registration WHERE uid = '.dbMakeSafe($uid).') UNION ';
+$sql .= 'SELECT DISTINCT cid, name FROM competition JOIN match USING (cid) LEFT JOIN registration USING (cid)';
+// registered users can see a competition when picks are ready for picking, non registered when awaiting results
+$sql .= ' WHERE cid <> '.dbMakeSafe($cid).' AND match.status > CASE WHEN uid IS NULL THEN 2 ELSE 1 END CASE ;';
+$result = dbQuery($sql);
+
+if (dbNumRows($result) > 0) {
+	if (!$menu) {
+?><ul id="menu">
+<?php
+		$menu=true;
+	}
+?>	<li>Competitions
+		<ul>
+<?php 
+	while ($row = dnFetchRow($result)) {
+?>			<li><a href="index.php?<?php echo 'cid='.$row['cid'] ; ?>"><?php echo $row['name'] ;?></a></li>
+<?php	}
+?>		</ul>
+	</li>
+<?php
+}
+if(in_array(SMF_FOOTBALL,$groups)) {
+// Am Global Administrator - let me also do Admin thinks
+	if (!$menu) {
+?><ul id="menu">
+<?php
+		$menu=true;
+	}
+?>	<li><a href="admin.php?<?php echo 'uid='.$uid.'&pass='.$password.'&global=true';?>">Administration</a></li>
+<?php 
+}
+
+if ($menu) {
+?></ul>
+<?php
+}
+if ($registered) {
+// If user is registered and we can do picks then we need to display the  Picks Section
+?><div id="picks">
+</div>
+<div id="bonus">
+</div>
+<?php
+}
 <div id="copyright">MBball <span id="version"></span> &copy; 2008 Alan Chandler.  Licenced under the GPL</div>
 </div>
 </body>
