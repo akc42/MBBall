04b8de2f2c8447d1f9a15af395362eecdd00fb87
Alan Chandler <alan@chandlerfamily.org.uk>
2008-08-17 21:04:57 +0100

    Fix problem with bonus score not checking uid

--- database.sql
+++ database.sql
@@ -266,7 +266,10 @@ COMMENT ON VIEW match_score IS 'points user scored in a match from the pick and 
 
 
 CREATE VIEW bonus_score AS
-    SELECT r.cid, r.rid, u.uid, (CASE WHEN p.uid IS NULL THEN 0 ELSE 1 END * r.value) AS score FROM ((registration u JOIN round r USING (cid)) LEFT JOIN option_pick p ON (((((p.cid = r.cid) AND (p.rid = r.rid)) AND (p.opid = r.answer)) AND (r.valid_question IS TRUE)))) WHERE r.open IS TRUE;;
+    SELECT r.cid, r.rid, u.uid, (CASE WHEN p.uid IS NULL THEN 0 ELSE 1 END * r.value) AS score
+	FROM ((registration u JOIN round r USING (cid))
+	LEFT JOIN option_pick p ON (((((p.cid = r.cid) AND (p.rid = r.rid)) AND (p.uid = u.uid) AND (p.opid = r.answer)) AND (r.valid_question IS TRUE))))
+	WHERE r.open IS TRUE;;
 
 COMMENT ON VIEW bonus_score IS 'Points scored in round by user answering the bonus question';
 
