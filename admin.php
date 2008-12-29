b9c9a1e4f509c3de6ed5c758b4e9c3ea2164ae17
Alan Chandler <alan@chandlerfamily.org.uk>
2008-12-29 16:07:53 +0000

    Add google analytics page tracker to Admin page

--- admin.php
+++ admin.php
@@ -32,6 +32,17 @@ require_once('db.php');
 </head>
 <body>
 <script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-xxxxxxx-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+
+<script type="text/javascript">
 	<!--
 
 var MBBmgr;
