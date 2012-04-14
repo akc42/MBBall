837e83fc5e04ddc2a8837d929f1a9f2a5fed6ed8
Alan Chandler <alan@chandlerfamily.org.uk>
2012-04-14 19:44:38 +0100

    Update dump to dump data in a single database called ./db/football.db
    This contains all competitions (like its Postgres Brother).

--- app/dump.php
+++ app/dump.php
@@ -34,9 +34,8 @@
 
 
 
-$db=new PDO('sqlite:./db/football.ini');
-$db->exec(file_get_contents('./db/football.sql'));  //setup first database
-$db = null;
+$db=new PDO('sqlite:./db/football.db');
+$db->exec(file_get_contents('./db/database.sql'));  //setup database
 
 define ('BALL',1);   //defined so we can control access to some of the files.
 require_once('db.inc');
@@ -45,33 +44,27 @@ dbQuery('BEGIN;');
 $result=dbQuery("SELECT cid FROM default_competition LIMIT 1");
 $row = dbFetchRow($result);
 $cid = $row['cid'];
+
+$db->exec('BEGIN EXCLUSIVE');
+$f=$db->prepare("UPDATE default_competition SET cid = ?");
+$f->bindValue(1,$row['cid']);
+$f->execute();
+$f->closeCursor();
+unset($f);
+
 dbFree($result);
+
+/*
+ * Set up competition table
+ */
 
 $result = dbQuery("SELECT *, date_part('epoch',creation_date) AS cd FROM competition");
 while($row = dbFetchRow($result)) {
-	$filename = './db/'.str_replace(Array(" ","."),Array('-','_'),$row['description']);
-	if(!file_exists($filename.'.db')) {
-		/*
-			Setup the main competition section of the database
-		*/
-		$newcid = uniqid(); //Generate a uniqid for this competition
-		$db = new PDO('sqlite:'.$filename.'.db');
-		$db->exec(file_get_contents('./db/database.sql')); //setup the database (this is within its own transaction)
-		if($row['cid'] == $cid) {
-			$db->exec("ATTACH './db/football.ini' AS football");
-			$f=$db->prepare("UPDATE football.config SET default_competition = ?, admin_key = NULL");
-			$f->bindValue(1,$newcid);
-			$f->execute();
-			$f->closeCursor();
-			unset($f);
-			$db->exec("DETACH football");
-		}
-		$db->exec("BEGIN EXCLUSIVE");
 		$c=$db->prepare("
-			INSERT INTO competition(cid,description,condition,administrator,open,pp_deadline,gap,bb_approval,creation_date,dversion)
-			VALUES (?,?,?,?,?,?,?,?,?,1)
+			INSERT INTO competition(cid,description,condition,administrator,open,pp_deadline,gap,bb_approval,creation_date)
+			VALUES (?,?,?,?,?,?,?,?,?)
 			");
-		$c->bindValue(1,$newcid);  //Give this competition a unique id (as near as possible)
+		$c->bindValue(1,$row['cid']);
 		$c->bindValue(2,$row['description']);
 		$c->bindValue(3,$row['condition']);
 		$c->bindValue(4,$row['administrator'],PDO::PARAM_INT);
@@ -83,171 +76,191 @@ while($row = dbFetchRow($result)) {
 		$c->execute();
 		$c->closeCursor();
 		unset($c);
-		/*
-			Setup participant table
-		*/
-		$table=dbQuery("SELECT u.*, agree_time,bb_approved FROM participant u JOIN registration r USING (uid) WHERE r.cid = ".$row['cid']);
-		$p=$db->prepare("
-			INSERT INTO participant(uid,name,email,password,last_logon,admin_experience,is_guest,agree_time,approved)
-			VALUES (?,?,?,null,?,?,?,?,?)
-			");
-		while($r2=dbFetchRow($table)) {
-			$p->bindValue(1,$r2['uid'],PDO::PARAM_INT);
-			$p->bindValue(2,$r2['name']);
-			$p->bindValue(3,$r2['email']);
-			$p->bindValue(4,$r2['last_logon'],PDO::PARAM_INT);
-			$p->bindValue(5,($r2['admin_experience'] == 't')?1:0,PDO::PARAM_INT);
-			$p->bindValue(6,($r2['is_bb'] == 't')?1:0,PDO::PARAM_INT);
-			$p->bindValue(7,$r2['agree_time'],PDO::PARAM_INT);
-			$p->bindValue(8,($r2['approved'] == 't')?1:0,PDO::PARAM_INT);
-			$p->execute();
-			$p->closeCursor();
-		}
-		dbFree($table);
-		unset($p);
-		/*
-			Setup Rounds
-		*/
-		$table=dbQuery("SELECT * FROM round WHERE cid = ".$row['cid']);
-		$r=$db->prepare("
-			INSERT INTO round(rid,question,valid_question,answer,value,name,ou_round,deadline,open)
-			VALUES (?,?,?,?,?,?,?,?,?)
-			");
-		while($r2=dbFetchRow($table)) {
-			$r->bindValue(1,$r2['rid'],PDO::PARAM_INT);
-			$r->bindValue(2,$r2['question']);
-			$r->bindValue(3,($r2['valid_question'] == 't')?1:0,PDO::PARAM_INT);
-			$r->bindValue(4,$r2['answer'],PDO::PARAM_INT);
-			$r->bindValue(5,$r2['value'],PDO::PARAM_INT);
-			$r->bindValue(6,$r2['name']);
-			$r->bindValue(7,($r2['ou_round'] == 't')?1:0,PDO::PARAM_INT);
-			$r->bindValue(8,$r2['deadline'],PDO::PARAM_INT);
-			$r->bindValue(9,($r2['open'] == 't')?1:0,PDO::PARAM_INT);
-			$r->execute();
-			$r->closeCursor();
-		}
-		dbFree($table);
-		unset($r);
-		/*
-			Setup Matches
-		*/
-		$table=dbQuery("SELECT * FROM match WHERE cid = ".$row['cid']);
-		$m=$db->prepare("
-			INSERT INTO match(rid,hid,aid,comment,ascore,hscore,combined_score,open,match_time)
-			VALUES (?,?,?,?,?,?,?,?,?)
-			");
-		while($r2=dbFetchRow($table)) {
-			$m->bindValue(1,$r2['rid'],PDO::PARAM_INT);
-			$m->bindValue(2,$r2['hid']);
-			$m->bindValue(3,$r2['aid']);
-			$m->bindValue(4,$r2['comment']);
-			$m->bindValue(5,$r2['ascore'],PDO::PARAM_INT);
-			$m->bindValue(6,$r2['hscore'],PDO::PARAM_INT);
-			$m->bindValue(7,$r2['combined_score'],PDO::PARAM_INT);
-			$m->bindValue(8,($r2['open'] == 't')?1:0,PDO::PARAM_INT);
-			$m->bindValue(9,$r2['match_time'],PDO::PARAM_INT);
-			$m->execute();
-			$m->closeCursor();
-		}
-		dbFree($table);
-		unset($m);
-		/*
-			Setup Option
-		*/
-		$table=dbQuery("SELECT * FROM option WHERE cid = ".$row['cid']);
-		$o=$db->prepare("INSERT INTO option(rid,opid,label) VALUES (?,?,?);");
-		while($r2=dbFetchRow($table)) {
-			$o->bindValue(1,$r2['rid'],PDO::PARAM_INT);
-			$o->bindValue(2,$r2['opid'],PDO::PARAM_INT);
-			$o->bindValue(3,$r2['label']);
-			$o->execute();
-			$o->closeCursor();
-		}
-		dbFree($table);
-		unset($o);
-		/*
-			Fixup (ie just adjust) Team
-		*/
-		$table=dbQuery("SELECT t.*,made_playoff FROM team t LEFT JOIN team_in_competition USING (tid) WHERE team_in_competition.cid = ".$row['cid']);
-		$t=$db->prepare("UPDATE team SET in_competition = ? , made_playoff = ? WHERE tid = ?");
-		while($r2=dbFetchRow($table)) {
-			$t->bindValue(1,(is_null($r2['made_playoff']))?0:1,PDO::PARAM_INT);
-			$t->bindValue(2,($r2['made_playoff'] == 't')?1:0,PDO::PARAM_INT);
-			$t->bindValue(3,$r2['tid']);
-			$t->execute();
-			$t->closeCursor();
-		}
-		dbFree($table);
-		unset($t);
-		/*
-			Setup Match Picks
-		*/
-		$table=dbQuery("SELECT * FROM pick WHERE cid = ".$row['cid']);
-		$p=$db->prepare("INSERT INTO pick(uid,rid,hid,comment,pid,over_selected,submit_time) VALUES (?,?,?,?,?,?,?)");
-		while($r2=dbFetchRow($table)) {
-			$p->bindValue(1,$r2['uid'],PDO::PARAM_INT);
-			$p->bindValue(2,$r2['rid'],PDO::PARAM_INT);
-			$p->bindValue(3,$r2['hid']);
-			$p->bindValue(4,$r2['comment']);
-			$p->bindValue(5,$r2['pid']);
-			$p->bindValue(6,($r2['over_selected'] == 't')?1:0,PDO::PARAM_INT);
-			$p->bindValue(7,$r2['submit_time'],PDO::PARAM_INT);
-			$p->execute();
-			$p->closeCursor();
-		}
-		dbFree($table);
-		/*
-			Setup Divisional Winner Picks
-		*/
-		$table=dbQuery("SELECT * FROM div_winner_pick WHERE cid = ".$row['cid']);
-		$p=$db->prepare("INSERT INTO div_winner_pick(uid,confid,divid,tid,submit_time) VALUES (?,?,?,?,?)");
-		while($r2=dbFetchRow($table)) {
-			$p->bindValue(1,$r2['uid'],PDO::PARAM_INT);
-			$p->bindValue(2,$r2['confid']);
-			$p->bindValue(3,$r2['divid']);
-			$p->bindValue(4,$r2['tid']);
-			$p->bindValue(5,$r2['submit_time'],PDO::PARAM_INT);
-			$p->execute();
-			$p->closeCursor();
-		}
-		dbFree($table);
-		/*
-			Setup WildCard Picks
-		*/
-		$table=dbQuery("SELECT * FROM wildcard_pick WHERE cid = ".$row['cid']);
-		$p=$db->prepare("INSERT INTO wildcard_pick(uid,confid,opid,tid,submit_time) VALUES (?,?,?,?,?)");
-		while($r2=dbFetchRow($table)) {
-			$p->bindValue(1,$r2['uid'],PDO::PARAM_INT);
-			$p->bindValue(2,$r2['confid']);
-			$p->bindValue(3,$r2['opid'],PDO::PARAM_INT);
-			$p->bindValue(4,$r2['tid']);
-			$p->bindValue(5,$r2['submit_time'],PDO::PARAM_INT);
-			$p->execute();
-			$p->closeCursor();
-		}
-		dbFree($table);
-		/*
-			Setup Option Picks
-		*/
-		$table=dbQuery("SELECT * FROM option_pick WHERE cid = ".$row['cid']);
-		$p=$db->prepare("INSERT INTO option_pick(uid,rid,opid,comment,submit_time) VALUES (?,?,?,?,?)");
-		while($r2=dbFetchRow($table)) {
-			$p->bindValue(1,$r2['uid'],PDO::PARAM_INT);
-			$p->bindValue(2,$r2['rid'],PDO::PARAM_INT);
-			$p->bindValue(3,$r2['opid'],PDO::PARAM_INT);
-			$p->bindValue(4,$r2['comment']);
-			$p->bindValue(5,$r2['submit_time'],PDO::PARAM_INT);
-			$p->execute();
-			$p->closeCursor();
-		}
-		dbFree($table);
-		unset($p);
-		$db->exec("COMMIT");
-		$db = null; //closes the database		
-	} else {
-		echo "The database $filename already exists\n";
-	}
 }
 dbFree($result);
+/*
+	Setup participant table
+*/
+$result=dbQuery("SELECT * FROM participant ");
+$p=$db->prepare("
+	INSERT INTO participant(uid,name,email,password,last_logon,admin_experience,is_guest)
+	VALUES (?,?,?,null,?,?,?)
+	");
+while($r2=dbFetchRow($result)) {
+	$p->bindValue(1,$r2['uid'],PDO::PARAM_INT);
+	$p->bindValue(2,$r2['name']);
+	$p->bindValue(3,$r2['email']);
+	$p->bindValue(4,$r2['last_logon'],PDO::PARAM_INT);
+	$p->bindValue(5,($r2['admin_experience'] == 't')?1:0,PDO::PARAM_INT);
+	$p->bindValue(6,($r2['is_bb'] == 't')?1:0,PDO::PARAM_INT);
+	$p->execute();
+	$p->closeCursor();
+}
+dbFree($result);
+unset($p);
+/*
+	Setup registration table
+*/
+$result=dbQuery("SELECT * FROM registration ");
+$r=$db->prepare("
+	INSERT INTO registration(cid,uid,agree_time,approved)
+	VALUES (?,?,?,?)
+	");
+while($r2=dbFetchRow($result)) {
+	$r->bindValue(1,$r2['cid'],PDO::PARAM_INT);
+	$r->bindValue(2,$r2['uid'],PDO::PARAM_INT);
+	$r->bindValue(3,$r2['agree_time'],PDO::PARAM_INT);
+	$r->bindValue(4,($r2['approved'] == 't')?1:0,PDO::PARAM_INT);
+	$r->execute();
+	$r->closeCursor();
+}
+dbFree($result);
+unset($r);
+/*
+	Setup Rounds
+*/
+$result=dbQuery("SELECT * FROM round");
+$r=$db->prepare("
+	INSERT INTO round(cid,rid,question,valid_question,answer,value,name,ou_round,deadline,open)
+	VALUES (?,?,?,?,?,?,?,?,?,?)
+	");
+while($r2=dbFetchRow($result)) {
+	$r->bindValue(1,$r2['cid'],PDO::PARAM_INT);
+	$r->bindValue(2,$r2['rid'],PDO::PARAM_INT);
+	$r->bindValue(3,$r2['question']);
+	$r->bindValue(4,($r2['valid_question'] == 't')?1:0,PDO::PARAM_INT);
+	$r->bindValue(5,$r2['answer'],PDO::PARAM_INT);
+	$r->bindValue(6,$r2['value'],PDO::PARAM_INT);
+	$r->bindValue(7,$r2['name']);
+	$r->bindValue(8,($r2['ou_round'] == 't')?1:0,PDO::PARAM_INT);
+	$r->bindValue(9,$r2['deadline'],PDO::PARAM_INT);
+	$r->bindValue(10,($r2['open'] == 't')?1:0,PDO::PARAM_INT);
+	$r->execute();
+	$r->closeCursor();
+}
+dbFree($result);
+unset($r);
+/*
+	Setup Matches
+*/
+$result=dbQuery("SELECT * FROM match");
+$m=$db->prepare("
+	INSERT INTO match(cid,rid,hid,aid,comment,ascore,hscore,combined_score,open,match_time)
+	VALUES (?,?,?,?,?,?,?,?,?,?)
+	");
+while($r2=dbFetchRow($result)) {
+	$m->bindValue(1,$r2['cid'],PDO::PARAM_INT);
+	$m->bindValue(2,$r2['rid'],PDO::PARAM_INT);
+	$m->bindValue(3,$r2['hid']);
+	$m->bindValue(4,$r2['aid']);
+	$m->bindValue(5,$r2['comment']);
+	$m->bindValue(6,$r2['ascore'],PDO::PARAM_INT);
+	$m->bindValue(7,$r2['hscore'],PDO::PARAM_INT);
+	$m->bindValue(8,$r2['combined_score'],PDO::PARAM_INT);
+	$m->bindValue(9,($r2['open'] == 't')?1:0,PDO::PARAM_INT);
+	$m->bindValue(10,$r2['match_time'],PDO::PARAM_INT);
+	$m->execute();
+	$m->closeCursor();
+}
+dbFree($result);
+unset($m);
+/*
+	Setup Option
+*/
+$result=dbQuery("SELECT * FROM option");
+$o=$db->prepare("INSERT INTO option(cid,rid,opid,label) VALUES (?,?,?,?);");
+while($r2=dbFetchRow($result)) {
+	$o->bindValue(1,$r2['cid'],PDO::PARAM_INT);
+	$o->bindValue(2,$r2['rid'],PDO::PARAM_INT);
+	$o->bindValue(3,$r2['opid'],PDO::PARAM_INT);
+	$o->bindValue(4,$r2['label']);
+	$o->execute();
+	$o->closeCursor();
+}
+dbFree($result);
+unset($o);
+/*
+	Setup Team In Competition (Team already setup)
+*/
+$result=dbQuery("SELECT * FROM team_in_competition ");
+$t=$db->prepare("INSERT INTO team_in_competition(cid,tid,made_playoff) VALUES (?,?,?)");
+while($r2=dbFetchRow($result)) {
+	$t->bindValue(1,$r2['cid'],PDO::PARAM_INT);	
+	$t->bindValue(2,$r2['tid']);
+	$t->bindValue(3,(is_null($r2['made_playoff']))?0:1,PDO::PARAM_INT);
+	$t->execute();
+	$t->closeCursor();
+}
+dbFree($result);
+unset($t);
+/*
+	Setup Match Picks
+*/
+$result=dbQuery("SELECT * FROM pick");
+$p=$db->prepare("INSERT INTO pick(cid,uid,rid,hid,comment,pid,over_selected,submit_time) VALUES (?,?,?,?,?,?,?,?)");
+while($r2=dbFetchRow($result)) {
+	$p->bindValue(1,$r2['cid'],PDO::PARAM_INT);
+	$p->bindValue(2,$r2['uid'],PDO::PARAM_INT);
+	$p->bindValue(3,$r2['rid'],PDO::PARAM_INT);
+	$p->bindValue(4,$r2['hid']);
+	$p->bindValue(5,$r2['comment']);
+	$p->bindValue(6,$r2['pid']);
+	$p->bindValue(7,($r2['over_selected'] == 't')?1:0,PDO::PARAM_INT);
+	$p->bindValue(8,$r2['submit_time'],PDO::PARAM_INT);
+	$p->execute();
+	$p->closeCursor();
+}
+dbFree($result);
+/*
+	Setup Divisional Winner Picks
+*/
+$result=dbQuery("SELECT * FROM div_winner_pick");
+$p=$db->prepare("INSERT INTO div_winner_pick(cid,uid,confid,divid,tid,submit_time) VALUES (?,?,?,?,?,?)");
+while($r2=dbFetchRow($result)) {
+	$p->bindValue(1,$r2['cid'],PDO::PARAM_INT);
+	$p->bindValue(2,$r2['uid'],PDO::PARAM_INT);
+	$p->bindValue(3,$r2['confid']);
+	$p->bindValue(4,$r2['divid']);
+	$p->bindValue(5,$r2['tid']);
+	$p->bindValue(6,$r2['submit_time'],PDO::PARAM_INT);
+	$p->execute();
+	$p->closeCursor();
+}
+dbFree($result);
+/*
+	Setup WildCard Picks
+*/
+$result=dbQuery("SELECT * FROM wildcard_pick");
+$p=$db->prepare("INSERT INTO wildcard_pick(cid,uid,confid,opid,tid,submit_time) VALUES (?,?,?,?,?,?)");
+while($r2=dbFetchRow($table)) {
+	$p->bindValue(1,$r2['cid'].PDO::PARAM_INT);
+	$p->bindValue(2,$r2['uid'],PDO::PARAM_INT);
+	$p->bindValue(3,$r2['confid']);
+	$p->bindValue(4,$r2['opid'],PDO::PARAM_INT);
+	$p->bindValue(5,$r2['tid']);
+	$p->bindValue(6,$r2['submit_time'],PDO::PARAM_INT);
+	$p->execute();
+	$p->closeCursor();
+}
+dbFree($result);
+/*
+	Setup Option Picks
+*/
+$result=dbQuery("SELECT * FROM option_pick");
+$p=$db->prepare("INSERT INTO option_pick(cid,uid,rid,opid,comment,submit_time) VALUES (?,?,?,?,?,?)");
+while($r2=dbFetchRow($table)) {
+	$p->bindValue(1,$r2['cid'],PDO::PARAM_INT);
+	$p->bindValue(2,$r2['uid'],PDO::PARAM_INT);
+	$p->bindValue(3,$r2['rid'],PDO::PARAM_INT);
+	$p->bindValue(4,$r2['opid'],PDO::PARAM_INT);
+	$p->bindValue(5,$r2['comment']);
+	$p->bindValue(6,$r2['submit_time'],PDO::PARAM_INT);
+	$p->execute();
+	$p->closeCursor();
+}
+dbFree($result);
+unset($p);
+$db->exec("COMMIT");
+$db = null; //closes the database		
 dbQuery('ROLLBACK');
 
